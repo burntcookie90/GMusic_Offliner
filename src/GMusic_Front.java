771adb5e6ca50d98bf10fafa61d53fd88204f493
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2012-07-22 12:24:08 -0400

    sql querying works

--- src/GMusic_Front.java
+++ src/GMusic_Front.java
@@ -3,6 +3,11 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -10,20 +15,19 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
 
-
 public class GMusic_Front extends JFrame {
+	private static Connection conn;
 	File dbfile;
 	public File[] fileList;
+	public Statement stat;
+	public ResultSet rs;
 	/**
 	 * @author vishnu
 	 *
 	 */
 	private class ButtonListener implements ActionListener {
-
-
 
 		/* (non-Javadoc)
 		 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
@@ -36,7 +40,13 @@ public class GMusic_Front extends JFrame {
 
 				if (returnVal == JFileChooser.APPROVE_OPTION) {
 					dbfile = fc.getSelectedFile();
-					lblDbFile.setText(dbfile.getName());
+					lblDbFile.setText(dbfile.getAbsolutePath());
+					try {
+						conn = DriverManager.getConnection("jdbc:sqlite:"+dbfile.getAbsolutePath());
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
 					//This is where a real application would open the file.
 				} else {
 				}
@@ -49,17 +59,41 @@ public class GMusic_Front extends JFrame {
 					cache_dir = fc2.getSelectedFile();
 					fileList = cache_dir.listFiles();
 					for(int i = 0; i<fileList.length;i++){
-						System.out.println(fileList[i]);
+						String curFileName = fileList[i].getName();
+						
+						System.out.println(curFileName);
+						
+						
+						
 					}
 					txtDirectory.setText(cache_dir.getAbsolutePath());
 					//This is where a real application would open the file.
 				} else {
 				}
 			}
-
+			else{
+				for(int i = 0; i<fileList.length;i++){
+					File curFile = fileList[i];
+					String curFileName = curFile.getName();
+					
+					System.out.println(curFileName);
+					try {
+						stat = conn.createStatement();
+						String sql = "select TrackNumber, Album, Genre, Artist, Title from music where localcopytype = 200 and LocalCopyPath = \""+curFileName+"\";";
+						System.out.println(sql);
+						rs = stat.executeQuery(sql);
+						while (rs.next()) {
+						      System.out.println("Album = " + rs.getString("Album"));
+						    }
+						
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+				}
+			}
 		}
-
-
 	}
 
 	final JFileChooser fc = new JFileChooser();
@@ -81,9 +115,14 @@ public class GMusic_Front extends JFrame {
 
 	/**
 	 * Launch the application.
+	 * @throws ClassNotFoundException 
+	 * @throws SQLException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
+		Class.forName("org.sqlite.JDBC");
+	    
+	    
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -118,7 +157,7 @@ public class GMusic_Front extends JFrame {
 		System.out.println("added button1");
 		lblDbFile = new JLabel("");
 		lblDbFile.setForeground(new Color(51,181,229));
-		lblDbFile.setBounds(44, 91, 70, 15);
+		lblDbFile.setBounds(12, 91, 265, 15);
 		contentPane.add(lblDbFile);
 
 		btnSelectMusicDirectory = new JButton("Select Music Directory");
@@ -145,7 +184,7 @@ public class GMusic_Front extends JFrame {
 		txtDirectory.setLineWrap(true);
 		txtDirectory.setForeground(new Color(51,181,229));
 		txtDirectory.setBackground(new Color(19,22,25));
-		txtDirectory.setBounds(45, 148, 199, 37);
+		txtDirectory.setBounds(12, 148, 265, 37);
 		contentPane.add(txtDirectory);
 	}
 }
